ff6b4c22b6f98b3823442b4a6b609262fb716908
Srinivasa Wudaru <swudaru@Srinivasas-Mac-mini.local>
2023-07-02 17:12:12 -0500

    Updated some fixes

---
+++ src/main/java/me/shreyeschekuru/dsa/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package me.shreyeschekuru.dsa.data.service;
+
+import me.shreyeschekuru.dsa.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    User findByUsername(String username);
+}
